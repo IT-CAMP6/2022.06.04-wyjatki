db436bd6957ffff3a85fe41ee5c9a558480f8d35
Mateusz Bereda <mat.bereda@gmail.com>
2022-06-04 22:13:02 +0200

    init

---
+++ src/pl/camp/it/Main.java
@@ -0,0 +1,43 @@
+package pl.camp.it;
+
+public class Main {
+    public static void main(String[] args) {
+
+        cos();
+
+        /*try {
+            cos();
+        } catch (MojWyjatek w) {
+            System.out.println("polecial nowy wyjatek !!!");
+            w.printStackTrace();
+        }*/
+        /*String input = "ysadgkfjhasd";
+
+        try {
+            int i = Integer.parseInt(input);
+        } catch (NumberFormatException blont) {
+            System.out.println("podałeś z dupy stringa !!!");
+            blont.printStackTrace();
+        }*/
+
+        /*try {
+            int[] tab = new int[5];
+
+            System.out.println(tab[5]);
+
+            Object object = cos();
+            object.toString();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Zepsuło się !!!");
+        } catch (Exception e) {
+            System.out.println("Zepsuł nię null !!!");
+        }
+
+
+        System.out.println("Cos sie działo dalej !!!");*/
+    }
+
+    public static Object cos() {
+        throw new MojWyjatek();
+    }
+}
